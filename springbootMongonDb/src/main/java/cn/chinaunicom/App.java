f865e7c3fa439f4dce7c142dc7f1411f221d424b
杨立新 <yanglx90@chinaunicom.cn>
2019-11-21 17:03:32 +0800

    mongodbDemo

---
+++ springbootMongonDb/src/main/java/cn/chinaunicom/App.java
@@ -0,0 +1,15 @@
+package cn.chinaunicom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ @author 杨立新
+ @date 2019/11/21 16:30
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run( App.class, args );
+    }
+}
---
+++ springbootMongonDb/src/main/java/cn/chinaunicom/model/MongoTest.java
@@ -0,0 +1,14 @@
+package cn.chinaunicom.model;
+
+import lombok.Data;
+
+/***
+ @author 杨立新
+ @date 2019/11/21 16:28
+ */
+@Data
+public class MongoTest {
+    private Integer id;
+    private Integer age;
+    private String name;
+}
